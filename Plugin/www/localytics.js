379eab92777575fa5a18fdc0d2fcd9c1de60a6be
Ayal Spitz <aspitz@localytics.com>
2015-07-30 11:25:19 -0400

    Added copyright info

--- Plugin/www/localytics.js
+++ Plugin/www/localytics.js
@@ -1,7 +1,12 @@
 //
 //  Localytics.js
+//  Copyright (C) 2015 Char Software Inc., DBA Localytics
 //
-//  Copyright 2015 Localytics. All rights reserved.
+//  This code is provided under the Localytics Modified BSD License.
+//  A copy of this license has been distributed in a file called LICENSE
+//  with this source code.
+//
+// Please visit www.localytics.com for more information.
 //
 
 var Localytics = function () {
@@ -110,8 +115,8 @@ Localytics.prototype.isOptedOut = function (successCallback) {
  Localytics.prototype.deleteProfileAttribute = function (name, scope) {
 	cordova.exec(null, null, "LocalyticsPlugin", "deleteProfileAttribute", [name, scope]);
 }
- 
- 
+
+
 /*******************
  * User Information
  ******************/
